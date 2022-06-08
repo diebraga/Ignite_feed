046a72108f1b49da0599ab732feb22b13abbffc2
diebraga <diebraga0055@gmail.com>
2022-06-08 22:14:45 +0100

    header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Header } from "./components/Header"
 
 function App() {
 
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div>
+      <Header />
+      <button>Hello world</button>
     </div>
   )
 }
