af2dc57769be0b1c3928e2632835e53d0759c877
Diego Braga <diegobraga@hertz.com>
2022-06-16 12:44:17 +0100

    get props avatar

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,12 +1,17 @@
+import { ImgHTMLAttributes } from 'react'
 import styles from './Avatar.module.css'
 
-type AvatarProps = {
-  src: string;
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
   hasBorder: boolean;
 }
 
-export default function Avatar({ src, hasBorder }: AvatarProps) {
+export default function Avatar({ src, hasBorder, alt, ...rest }: AvatarProps) {
   return (
-    <img src={src} className={`${styles.avatar} ${hasBorder ? styles.hadBorder : null}`} />
+    <img
+      src={src}
+      className={`${styles.avatar} ${hasBorder ? styles.hadBorder : null}`}
+      alt={alt}
+      {...rest}
+    />
   )
 }
